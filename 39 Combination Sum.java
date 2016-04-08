f317979057758127036a4daff5dbc7b69c26875f
WorthyYao <worthyyao@163.com>
2016-04-08 11:06:19 +0800

    039 Combination Sum

---
+++ 39 Combination Sum.java
@@ -0,0 +1,61 @@
+// Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+// The same repeated number may be chosen from C unlimited number of times.
+
+// Note:
+// All numbers (including target) will be positive integers.
+// Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+// The solution set must not contain duplicate combinations.
+// For example, given candidate set 2,3,6,7 and target 7, 
+// A solution set is: 
+// [7] 
+// [2, 2, 3] 
+
+
+这是一道求一个数组中所有加起来成为target组合的问题，可以要求重复数字。这道题不会做，在查看了网上的各种解答后
+明白了是用dfs来做的。深度优先遍历这种算法，因为有递归的思想在里面，理解起来很是痛苦，
+最后通过了，但是感觉还是很抽象，以后还是要多做练习这种类型的题目。
+
+
+/**
+ *
+ *
+ *
+ */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(candidates == null || candidates.length == 0) return result;
+
+        ArrayList<Integer> current = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+
+        combinationSum(candidates, target, 0, current, result);
+
+        return result;
+    }
+
+    public void combinationSum(int[] candidates, int target, int j, ArrayList<Integer> curr, List<List<Integer>> result){
+        if(target == 0){
+            ArrayList<Integer> temp = new ArrayList<Integer>(curr);
+            result.add(temp);
+            return;
+        }
+
+        for(int i=j; i<candidates.length; i++){
+            if(target < candidates[i])
+                return;
+
+            curr.add(candidates[i]);
+            combinationSum(candidates, target - candidates[i], i, curr, result);
+            curr.remove(curr.size()-1);
+        }
+
+    }
+}
