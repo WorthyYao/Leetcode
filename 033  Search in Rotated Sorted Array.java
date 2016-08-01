e35c8b2a9d8ec18884c2d2a8ff94ea80b106c0d8
worthyyao <worthyyao@163.com>
2016-08-01 16:25:28 +0800

    033 Search in Rotated Sorted Array

--- 033  Search in Rotated Sorted Array.java
+++ 033  Search in Rotated Sorted Array.java
@@ -1,24 +1,38 @@
-// Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
 
-// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 
-// You are given a target value to search. If found in the array return its index, otherwise return -1.
+You are given a target value to search. If found in the array return its index, otherwise return -1.
 
-// You may assume no duplicate exists in the array.
+You may assume no duplicate exists in the array.
 
-//这道题的题目本身我不是太清楚，直接找出所需要的target所在位置不就可以了= =。
-
-
+这道题目的意思是需要在一个逆序的数组中，找出给出的目标数的位置来。
+如果是普通查找的话，遍历一次数组就可以了。非常的简单，但是o(n)的时间复杂度就不行了。
+典型的要用二分搜索来做。由于这道题目相较于普通的二分搜索来说的话，因为逆序了，所以要多一个判断。
+好在在每次二分的时候，都只有一边是逆序的，而另一边则是有序的，所以加一个判断就好了。思路是比较清晰的。
+*/
 public class Solution {
     public int search(int[] nums, int target) {
-        int index=-1;
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                index=i;
-                break;
+        int left=0,right=nums.length-1;
+        int mid;
+        while (left<=right){
+            mid=(left+right)/2;
+            if (target==nums[mid]) return mid;
+            if (nums[mid]<nums[right]){
+                if(target>nums[mid]&&target<=nums[right]){
+                    left=mid+1;
+                }else {
+                    right=mid-1;
+                }
+            }else {
+                if(target<nums[mid]&&target>=nums[left]){
+                    right=mid-1;
+                }else {
+                    left=mid+1;
+                }
             }
         }
-        return index;
+        return -1;
     }
 }
